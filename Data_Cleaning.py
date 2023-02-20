d3c726d979861702cd30b16b59cc07f86efe6b28
itunghsieh <itunghsieh@gmail.com>
2023-02-21 02:57:49 +0800

    added an option to delete the whole row if the data is empty

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -10,23 +10,25 @@ def clean_col_data(data, col_name, method):
         'ffill': data[col_name].fillna(method='ffill'),
         'mean': data[col_name].fillna(data[col_name].mean()),
         'avg': data[col_name].fillna((data[col_name].ffill() + data[col_name].bfill()) / 2),
-        '0': data[col_name].fillna(0)
+        '0': data[col_name].fillna(0),
+        'delete': data.dropna(subset=[col_name])
     }
     if method not in switcher.keys():
         print("The method from the config is wrong, using 'bfill' as default.")
-        return data[col_name].fillna(method='bfill')
-    return switcher[method]
+        method = 'bfill'
 
+    if method == 'delete':
+        data = switcher[method]
+    else:
+        data[col_name] = switcher[method]
+    return data
 
-path = r"C:\Users\itung\OneDrive\桌面\Data Science Midterm\Weather Data"
-
-all_files = glob.glob(os.path.join(path, "2011-12-combined.csv"))
+all_files = glob.glob(os.path.join(config['path'], f"{config['station_id']}-combined.csv"))
 
 for file in all_files:
     df = pd.read_csv(file)
-    # clean_col_data('Precp', config['method'], df)
-
     df = df.apply(pd.to_numeric, errors='coerce')
-    df['Precp'] = clean_col_data(df, 'Precp', config['method'])
 
-    df.to_csv(path + r'\test_file.csv', index=False)
+    df = clean_col_data(df, 'Precp', config['method'])
+
+    df.to_csv(config['path'] + r'\test_file.csv', index=False)
