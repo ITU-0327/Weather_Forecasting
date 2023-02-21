27881a027987a50c8833fc7db6639db422de6e11
itunghsieh <itunghsieh@gmail.com>
2023-02-21 22:26:56 +0800

    Added new cleaning method 'day_mean' for filling the data with the mean value of other data of the day

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config = {
     "remove_files": False,
-    "method": 'delete',  # bfill, ffill, mean, avg, 0, delete
+    "method": 'delete',  # bfill, ffill, mean, avg, 0, delete, day_mean
     "station_id": 'C0F9T0',
     "download_path": r"C:\Users\itung\OneDrive\桌面\Weather_Forecasting\Weather Raw Data",
     "path": r"C:\Users\itung\OneDrive\桌面\Weather_Forecasting\Weather Data"
