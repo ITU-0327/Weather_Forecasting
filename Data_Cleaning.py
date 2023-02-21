35bf0b8efc9c256099c7c0c1bb12c8a880f03380
itunghsieh <itunghsieh@gmail.com>
2023-02-21 22:29:53 +0800

    Add a method add_day() to add one day to the data and check if it's an invalid date and fix it. Add convert_time() to turn 24. to 0.

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from config import config
+import calendar
 
 
 def clean_col_data(data, col_name, method):
@@ -9,6 +10,7 @@ def clean_col_data(data, col_name, method):
         'ffill': data[col_name].fillna(method='ffill'),
         'mean': data[col_name].fillna(data[col_name].mean()),
         'avg': data[col_name].fillna((data[col_name].ffill() + data[col_name].bfill()) / 2),
+        'day_mean': data.groupby(['Year', 'Month', 'Day'])[col_name].transform(lambda x: x.fillna(round(x.mean(), 2))),
         '0': data[col_name].fillna(0),
         'delete': data.dropna(subset=[col_name])
     }
@@ -23,9 +25,30 @@ def clean_col_data(data, col_name, method):
     return data
 
 
+def add_day(data):
+    data['Day'] += 1
+    max_day = calendar.monthrange(data['Year'], data['Month'])[1]
+    if data['Day'] > max_day:
+        data['Day'] = 1
+        data['Month'] += 1
+        if data['Month'] > 12:
+            data['Month'] = 1
+            data['Year'] += 1
+    return data
+
+
+def convert_time(data):
+    if data['ObsTime'] == 24:
+        data = add_day(data)
+        data['ObsTime'] = 0
+    return data
+
+
 df = pd.read_csv(os.path.join(config['path'], f'{config["station_id"]}-combined.csv'))
+df = df.apply(convert_time, axis=1)
 df = df.apply(pd.to_numeric, errors='coerce')
 
-df = clean_col_data(df, 'Precp', config['method'])
+df = clean_col_data(df, 'Temperature', config['method'])
+df = clean_col_data(df, 'Precp', config['day_mean'])
 
 df.to_csv(os.path.join(config['path'], 'test_file.csv'), index=False)
